8ed8b1785478df0bd97e3aaadea28b91ce57fd22
임형진(Hyeong-Jin Im) <max804@naver.com>
2016-10-18 00:46:38 +0900

    코드 정리 및 Loop문 구현중...

--- src/GameServer/GameService.go
+++ src/GameServer/GameService.go
@@ -4,13 +4,18 @@ import (
     "os"
     "time"
     "fmt"
+    "net"
+    "strconv"
 )
 
 // --------------------------
-// GameService : 게임 세계의 연산을 담당하는 게임 서비스이다.
+// GameService : 게임 서버의 전체적인 처리를 하는 게임 서비스이다.
 type GameService struct {
-    ID int64
     logOutput *os.File
+
+    port int
+
+    connectClientCh chan net.Conn
 }
 
 // Start : 게임 서비스를 실행한다.
@@ -26,7 +31,12 @@ func (g *GameService) Start() {
     }
     g.logOutput = o
 
-   g.WriteLog(fmt.Sprintf("GameService(%d) Start.", g.ID))
+    // 클라이언트 Listen 열기
+    g.ListenClient()
+    // 메인 루프 실행
+    g.Loop()
+
+    g.WriteLog("GameService Start.")
 }
 
 // WriteLog : 로그파일에 로그를 남긴다.
@@ -39,12 +49,46 @@ func (g *GameService) WriteLog(log string) {
         panic(err)
     }
 }
+
+// ListenClient : 클라이언트 접속을 기다린다.
+func (g *GameService) ListenClient() {
+    // 설정한 Port로 Listen 한다.
+    listen, err := net.Listen("tcp4", ":" + strconv.Itoa(g.port))
+    defer listen.Close()
+    if err != nil {
+        g.WriteLog(fmt.Sprintf("Fail to listen on port %d.", g.port))
+       // g.WriteLog(err.(string))
+        return
+    }
+    g.WriteLog("Start to listen to connect Client...")
+
+    for {
+        conn, err := listen.Accept()
+        if err != nil {
+            g.WriteLog("Fail to accept the client.")
+        } else {
+            g.connectClientCh <- conn
+        }
+    }
+}
+
+// Loop : GameService의 메인 루프문
+func (g *GameService) Loop() {
+LOOP:
+   /* for {
+        select {
+            case conn := <-g.connectClientCh: // 클라이언트 연결 됨
+                g.WriteLog("Client connected.")
+                conn.
+        }
+    }*/
+}
 // ---------------------------
 
 // CreateGameService: 게임 서비스를 생성한다.
-func CreateGameService() *GameService {
+func CreateGameService(port int) *GameService {
     g := GameService {
-        ID:123,
+        port: port,
     }
 
     return &g
--- src/Main/Server.go
+++ src/Main/Server.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-    "fmt"
     gs "GameServer"
 )
 
 func main(){
     gameService := gs.CreateGameService()
     gameService.Start()
-    
-    gameService.WriteLog(fmt.Sprintf("Test Log(%s, %d, %f)", "T1", 123, 1.23))
 }
